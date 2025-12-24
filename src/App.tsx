93d900fe31403365dfd12ff02188b2b179dbdfa8
James Treap <james.treap@gmail.com>
2025-12-24 15:23:35 -0500

    Added popcorn

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Card from "./Card";
 import osmows from "/osmows.png";
 import craveable from "/craveable.png";
 import hotchocolate from "/hotchocolate.png";
+import popcorn from "/popcorn.png";
 import { QueryClientProvider } from "@tanstack/react-query";
 import LoginForm from "./loginForm";
 import { queryClient } from "./utils/queryClient";
@@ -23,6 +24,7 @@ function App() {
     { title: "Craveable", image: craveable },
     { title: "Osmows", image: osmows },
     { title: "Hot Chocolate", image: hotchocolate },
+    { title: "Popcorn", image: popcorn },
   ];
 
   return (
